cc4aa7b84d052dc71cda4476b0e8a8bd956d733d
AYAELALAMI <git@github.com>
2023-10-26 01:49:46 -0700

    rev rev

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * _puts_recursion - prints string
- * @s: string to print
- * Hello_recursion
+ * _print_rev_recursion - prints string in reverse
+ * @s: reverse string to print
+ * Return: void
  */
 
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s > '\0')
 	{
 		_print_rev_recursion(s + 1);
 		putchar(*s);
