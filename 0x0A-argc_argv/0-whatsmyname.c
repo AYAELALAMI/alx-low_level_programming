d480f564262e5d98a6c7bd552f2ead7c8ce2c5e3
AYAELALAMI <git@github.com>
2023-10-30 14:47:06 -0700

    easy argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - has and will print program name
+ * @argv: argument vector
+ * @argc: argument counter
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
