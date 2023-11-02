e5cf557e3241397ef3565ae51472bfc66ce3618a
AYAELALAMI <git@github.com>
2023-11-01 23:53:08 -0700

    more malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * *malloc_checked - allocates memeory using malloc and exit if failed
+ * @b: int
+ * Return: pointer to the array initialized or NULL
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *h = malloc(b);
+
+	if (h == 0)
+		exit(98);
+
+	return (h);
+}
