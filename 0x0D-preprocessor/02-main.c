fa9b93fbed49c2c1fd64a7bdcdef4ed347ee2d73
AYAELALAMI <git@github.com>
2023-11-06 14:28:48 -0800

    main.c

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/** main - is the entery point of the code
+ * declaration: print the name of the source file
+ *
+ * Return: 0 (success).
+ */
 int main() {
 	printf("%s\n", __FILE__);
 
