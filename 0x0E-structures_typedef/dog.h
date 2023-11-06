fe3a28e80243e023b5bae37b7204b1fec2486b25
AYAELALAMI <git@github.com>
2023-11-06 15:13:12 -0800

    doggie

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef dog_h
 #define dog_h
 
+/*
+ * struct dog - a struct of a dog
+ * @name: a string that contains the dog's name
+ * @age: a float that holds the dog's age
+ * @owner: the name of the owner
+ *
+ * description: is a description of a dog's owner age and name
+ */
 struct dog
 {
 	char *name;
