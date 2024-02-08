9ccd2f08275f06e219c21e02301e315ddc2cde5b
AYAELALAMI <eaya4570@gmail.com>
2024-02-08 14:38:35 -0800

    b

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,10 +11,15 @@ unsigned int binary_to_uint(const char *b)
 
 	unsigned int keep = 0;
 
-	if (b == NULL && b != 0 && *b != 1)
+	if (!b)
 		return (0);
-	while (*b != '\0')
-		keep = (keep * 2 + (*b++ - '0'));
 
+	while (*b != '\0')
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+
+		keep = (keep * 2 + (*b++ - '0'));
+	}
 				return (keep);
 }
