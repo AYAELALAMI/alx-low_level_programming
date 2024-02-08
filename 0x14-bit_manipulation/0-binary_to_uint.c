823ef4e03b868c4bca3e6e7ae0a3cbac39a32657
AYAELALAMI <eaya4570@gmail.com>
2024-02-08 11:35:12 -0800

    bit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * binary_to_uint - convert  binary number to an unsigned int
+ * @b: string of binary numbers
+ * Return: converted number
+ */
+
+unsigned int binary_to_uint(const char *b){
+	if ( b == NULL && b != 0 && b != 1)
+		return 0;
+	while (*b != '\0') {
+}
