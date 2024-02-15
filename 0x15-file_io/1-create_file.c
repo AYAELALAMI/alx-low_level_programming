098836aa33a3b12586e37d6770de9895d9b4bd9d
AYAELALAMI <eaya4570@gmail.com>
2024-02-15 15:27:22 -0800

    file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
+
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
 
 	if (text_content != NULL)
 		text_content = "";
@@ -26,6 +30,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (i == -1)
 		return (-1);
+
 	close(fd);
 	return (1);
 }
